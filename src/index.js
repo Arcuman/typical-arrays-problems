60d646974add5cec1c3d9ad82df61917b1b5be10
Arcuman <anton.arcumn.borisov@gmail.com>
2020-10-02 10:32:54 +0300

    feat: add max min avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.max = function min(array) {
+	if (array == null || array.length == 0)
+		return 0;
+	var maxValue = array[0];
+	for (var i = 1; i < array.length; i++) {
+		var currentValue = array[i];
+		if (currentValue > maxValue) {
+			maxValue = currentValue;
+		}
+	}
+	return maxValue;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.min = function max(array) {
+	if (array == null || array.length == 0)
+		return 0;
+	var minValue = array[0];
+	for (var i = 1; i < array.length; i++) {
+		var currentValue = array[i];
+		if (currentValue < minValue) {
+			minValue = currentValue;
+		}
+	}
+	return minValue;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	if (array == null || array.length == 0)
+		return 0;
+	var avg = 0;
+	for (var i = 0; i < array.length; i++) {
+		avg += array[i];
+	}
+	return avg / array.length;
 }
